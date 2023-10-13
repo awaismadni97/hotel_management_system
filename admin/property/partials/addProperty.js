055d86567630f672ee1097d7d05a233152b0e13a
awaismadni97 <awais.madni17@gmail.com>
2023-10-13 13:02:04 +0500

    property attributes update done

--- admin/property/partials/addProperty.js
+++ admin/property/partials/addProperty.js
@@ -2,8 +2,11 @@
 
 let id= null;
 let propertyImages=[];
+let propertyAttributesArray=[];
+let propertyAttributeObj =null;
 let arr =  JSON.parse(localStorage.getItem('propertyData'));
 
+//  get image file 
 $("#uploadFile").change(function(e){
    catchImageFile= e.target.files;
    for(let allFiles of catchImageFile){
@@ -17,94 +20,142 @@ $("#uploadFile").change(function(e){
 
 })
 
+      // get_property_attribute_data
 
-$("#propertyForm").validate({
-
-    errorClass: "error fail-alert",
-    validClass: "valid success-alert",
-
- rules:{
-    property_title:{
-        required:true
-    },
-    room_type:{
-        required:true
-    },
-    property_location:{
-        required:true
-    },
-    // uploadFile:{
-    //     required:true
-    // }
- },messages:{
-    property_title:{
-        required:'Title field is required'
-    },
-    room_type:{
-        required:'Room-type is required'
-    },
-    property_location:{                                                                                                 
-        required:'Location field is required'
-    },
-    // uploadFile:{
-    //     required:'Image field is required'
-    // }
-
- },
-
- submitHandler: function(form){
+$("#save_btn").click(function(){
+    $('#attrForm').submit(function(){
+      $('.modal').modal('hide');
+        //   let attr_array = JSON.parse(localStorage.getItem('property_attributes'));
+          
+          let roomLimit = $("#room_limit").val();
+          let BedType =$("#bed_Type").val();
+          let viewChoice=$('#view-choice').val();
+          let NonSmokeyRoom =$('[name=smoke_options]:checked').val();
+          let PetFriendly=$('[name=pet_options]:checked').val();
+          let airConditioningService= $('[name=ac_options]:checked').val();
+          let kitchenAvailablity=$('[name=kitchen_options]:checked').val();
+          let tvService = $('[name=Tv_options]:checked').val();
+          let roomService = $('[name=room_service]:checked').val();
+          let privateBathRooms =$('[name=bathroom_service]:checked').val();
+           
+           event.preventDefault();
+           propertyAttributeObj={
+            roomLimit:roomLimit,
+            BedType:BedType,
+            viewChoice:viewChoice,
+            NonSmokeyRoom:NonSmokeyRoom,
+            PetFriendly:PetFriendly,
+            airConditioningService:airConditioningService,
+            kitchenAvailablity:kitchenAvailablity,
+            tvService:tvService,
+            roomService:roomService,
+            privateBathRooms:privateBathRooms
+          }
+          
+          propertyAttributesArray.push(propertyAttributeObj);
     
-     propertyTitle= $("#property_title").val();
-     roomType= $("#room_type").val();
-     propertyLocation= $("#property_location").val();
-     PropertyDescription= $("#property_description").val();
-     pricingPlan =$("#price_plan").val();
-     PropertyImages= propertyImages
+ });
+    
+})
 
-    event.preventDefault();
 
-    propertyObj={
-        propertyTitle:propertyTitle,
-        roomType:roomType,
-        propertyLocation:propertyLocation,
-        PropertyDescription:PropertyDescription,
-        pricingPlan:pricingPlan,
-        PropertyImages:PropertyImages
-    }
 
-    $("#submit").text("Please wait..");
-    $("#submit").attr("disabled", true);
-    setTimeout(() => {
-        $("#submit").attr("disabled", false);
-        if(id==null){
-            if(arr==null){
-               let propData=[propertyObj];
-               setLocalData(propData);
-               $("#submit").text("Add");
-               sessionStorage.setItem("CreateSuccess", "true");
-                //  alert('Record has been created successfully');
-            }else{
-               arr.push(propertyObj);
-               setLocalData(arr);
-               $("#submit").text("Add");
-               sessionStorage.setItem("CreateSuccess", "true");
-            //    alert('Record has been created successfully');
-            }
-        }else{
-            //update Logic
-            updateData(id);
-              id=null;
-              $("#submit").text("Add");
-              sessionStorage.setItem("UpdateSuccess", "true");
-            //   alert('Record has been updated successfully');
+    $("#propertyForm").validate({
+        errorClass: "error fail-alert",
+        validClass: "valid success-alert",
+    
+     rules:{
+        property_title:{
+            required:true
+        },
+        room_type:{
+            required:true
+        },
+        property_location:{
+            required:true
+        },
+        price_plan:{
+            required:true
         }
-        resetData();
-        window.location = "displayProperty.html"
-    }, 1000);
+        // uploadFile:{
+        //     required:true
+        // }
+     },messages:{
+        property_title:{
+            required:'Title field is required'
+        },
+        room_type:{
+            required:'Room-type is required'
+        },
+        property_location:{                                                                                                 
+            required:'Location field is required'
+        },
+        price_plan:{
+            required:'Mention the price'
+        }
+        // uploadFile:{
+        //     required:'Image field is required'
+        // }
+        
+    },
+    
+     submitHandler: function(form){
+        // event.preventDefault();
+        alert('submit handler is called');
+         propertyTitle= $("#property_title").val();
+         roomType= $("#room_type").val();
+         propertyLocation= $("#property_location").val();
+         PropertyDescription= $("#property_description").val();
+         pricingPlan =$("#price_plan").val();
+         PropertyImages= propertyImages;
+         propertyAttributesArray:propertyAttributesArray 
+        
+        propertyObj={
+            propertyTitle:propertyTitle,
+            roomType:roomType,
+            propertyLocation:propertyLocation,
+            PropertyDescription:PropertyDescription,
+            pricingPlan:pricingPlan,
+            PropertyImages:PropertyImages,
+            propertyAttributesArray:propertyAttributesArray
+        }
+        event.preventDefault();     
+        $("#submit").text("Please wait..");
+        $("#submit").attr("disabled", true);
+        setTimeout(() => {
+            $("#submit").attr("disabled", false);
+            if(id==null){
+                if(arr==null){
+                   let propData=[propertyObj];
+                   setLocalData(propData);
+                   $("#submit").text("Add");
+                   sessionStorage.setItem("CreateSuccess", "true");
+                    //  alert('Record has been created successfully');
+                }else{
+                   arr.push(propertyObj);
+                   setLocalData(arr);
+                   $("#submit").text("Add");
+                   sessionStorage.setItem("CreateSuccess", "true");
+                //    alert('Record has been created successfully');
+                }
+            }else{
+                //update Logic
+                updateData(id);
+                  id=null;
+                  $("#submit").text("Add");
+                  sessionStorage.setItem("UpdateSuccess", "true");
+                //   alert('Record has been updated successfully');
+            }
+            resetData();
+            window.location = "displayProperty.html"
+        }, 1000);
+    
+     } 
+    
+    });
+ 
+   
 
- } 
-
-});
 
 
 //set Data in local Storage
@@ -167,8 +218,8 @@ function updateData(rid){
     arr[rid].propertyLocation=propertyLocation;
     arr[rid].PropertyDescription=PropertyDescription;
     // arr[rid].propertyImages[0].filename=propertyImages;
+    arr[rid].propertyAttributesArray=propertyAttributesArray;
     localStorage.setItem('propertyData', JSON.stringify(arr));
     
 }
-
 
